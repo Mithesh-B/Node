8106c8540a057ced1e252ca64e7ab455ecfacbc9
Mithesh B <mitheshbangera70@gmail.com>
2023-03-22 12:33:42 +0530

    global

---
+++ 2_globals.js
@@ -0,0 +1,15 @@
+// Globals - no window
+
+// __dirname -path to current directory
+// __filename - file name
+// require - function to use modules(common js)
+// module - info about current module
+// process - info of env where program is being executed
+console.log(__dirname)
+setTimeout(()=>{
+    console.log("hi there")
+},3000)
+
+setInterval(()=>{
+    console.log("hi there")
+},3000)
